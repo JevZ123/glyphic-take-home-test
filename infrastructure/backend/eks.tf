d1e49b99bcdf24852ddfac4492be7a51135bf711
Jevgenij Zubovskij <zubovskij@gmail.com>
2025-01-06 11:26:45 +0200

    logs

--- infrastructure/backend/eks.tf
+++ infrastructure/backend/eks.tf
@@ -3,6 +3,15 @@ resource "aws_eks_cluster" "eks_cluster" {
   name     = "eks-cluster"
   role_arn = aws_iam_role.eks_cluster_role.arn
 
+  enabled_cluster_log_types = [
+    "api",
+    "audit",
+    "authenticator",
+    "controllerManager",
+    "scheduler"
+  ]
+
+
   vpc_config {
     subnet_ids = [
       aws_subnet.public_subnet_a.id,
